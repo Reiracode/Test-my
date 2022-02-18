402be43f45a1905a6f37044dea1e3ddb7fdec66c
Reiracode <reira.igg@gmail.com>
2022-02-18 11:46:03 +0800

    update v2

--- js/format.js
+++ js/format.js
@@ -1,17 +1,17 @@
-// include-千分位
+//千分位
 function number_format(n) {
   n += "";
   let arr = n.split(".");
   let re = /(\d{1,3})(?=(\d{3})+$)/g;
   return arr[0].replace(re, "$1,") + (arr.length == 2 ? "." + arr[1] : "");
 }
+
 //去千分位號
 function RemoveComma(n) {
   return n.replace(/[,]+/g, "");
 }
 
 //login true or false
-
 const domainName = window.location.href;
 const loc = domainName.indexOf(":", 6);
 const allurl = domainName.slice(0, loc) + ":8080";
@@ -20,7 +20,7 @@ const allitem = !!(allurl == "http://localhost:8080")
   ? allurl + "/reirasys_api/shopitems.php"
   : "https://reiracode.github.io/ajax/allitems.json";
 
-const getProd = async () => {
+const getAllitems = async () => {
   const response = await fetch(allitem);
   const json = await response.json();
   return json
@@ -33,11 +33,6 @@ const getProd = async () => {
   // }
 };
 
-const FindApi = async () => {
-  const response = await fetch(allitem);
-  const json = await response.json();
-  return json;
-};
 //all items
 async function searchAPI() {
   return await fetch(`${allitem}`).then((data) => data.json());
@@ -58,57 +53,79 @@ if (url.indexOf("/") != -1) {
 }
 
 function process_color() {
-  if (add[0].indexOf("step") != -1) {
-    let step = add[0].split("_");
-     step = step[step.length - 1];
-    console.log(step.substr(4, 1));
+  site_map();
+
+  if ((myUrl.main).indexOf("step") != -1) {
+    let step = (myUrl.main).split("_");
+    step = step[step.length - 1];
 
     let now_step = step.substr(4, 1);
     for (let i = now_step - 1; i >= 0; i--) {
-      console.log(i);
-      // $('.process2 a div').eq(i).css({'backgroundColor':'#d08080','opacity':'1'});
-      $(".process3 li").eq(i).css({ backgroundColor: "#d08080", opacity: "1" });
+      $(".process3 li").eq(i).css({ backgroundColor: "#efc1c1", opacity: "1" });     
+    }
+
+    if (now_step == "2") {
+      $(".process3").attr("data-width", "large");
+    } else if (now_step == "3") { 
+      $(".process3").attr("data-width", "xlarge");
     }
   }
 }
 
-// login_status
+// function getLogin_format() {
+//   $.ajax({
+//     url: allurl + "/reirasys_api/is_login.php",
+//     type: "get",
+//     dataType: "json",
+//     success: function (data) {
+//       console.log(data);
+//     },
+//   });
+// }
+// getLogin_format();
+
+
+//判斷登入way2 * 建議用此作法，較清楚
+// 定義
+function getLogin_status() {
+  return $.ajax({
+    url: allurl + "/reirasys_api/is_login.php",
+    type: "get",
+    dataType: "json",
+    success: function (data) {
+      data.userno;
+    }
+  });
+}
+// ==>使用方式
+getLogin_status().then(function (data) {
+  console.log("getLogin_status:" + data.userno);
+});
+// ==>getLogin_status:000002
+
+
+// 判斷登入way1**
 let login_status = function () {
   return $.ajax({
     url: allurl + "/reirasys_api/is_login.php",
     type: "GET",
     dataType: "json",
   }).then(function (data) {
-    console.log(data);
+    // console.log(data);
     return data;
   });
 };
 
-function getLogin_format() {
-  $.ajax({
-    url: allurl + "/reirasys_api/is_login.php",
-    type: "get",
-    dataType: "json",
-    success: function (data) {
-      console.log(data.success);
-      // return data.success
-      return "data.success";
-    },
-  });
-}
-
-function getLogin_status() {
-  // 追加処理
-  // Ajaxで送信
-  return $.ajax({
-    url: allurl + "/reirasys_api/is_login.php",
-    type: "get",
-    dataType: "json",
-    success: function (data) {
-      data.success;
-    },
-  });
-}
+// ==>使用方式
+login_status().then(function (data) {
+    if (data.IsLogin) {
+        console.log(data);
+    }
+}).fail(function () {
+    console.log('err');
+});
+//==>呼び出し先 000002
+//==collections.html:59 login success sucess
 
 //form input all trim
 function formTrim(formValueall) {
